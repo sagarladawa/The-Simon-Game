8e65e666351cff0f9892ada02934812502ee37bc
Sagar Ladawa <me.sagar.ladawa@gmail.com>
2024-03-18 00:48:40 +0530

    JQuery refactor

--- src/game.js
+++ src/game.js
@@ -4,34 +4,40 @@ var buttonColors = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-// Add on-click event listener to buttons
-$(".btn").on("click", userButtonClick);
-
 nextSequence();
 
-function userButtonClick()
-{
-    var userChosenColor = this.id;
-    playSound(userChosenColor);
+// Add on-click event listener to buttons
+$(".btn").on("click", function () {
+    var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+
+function animatePress(color)
+{
+    var buttonElem = $("div#" + color);
+    buttonElem.addClass("pressed");
+    setTimeout(function (){
+        buttonElem.removeClass("pressed")
+    }, 150);
 }
 
 function nextSequence()
 {
+    userClickedPattern = [];
+
     var random = Math.floor(Math.random()*4);
     var randomChosenColor = buttonColors[random];
     gamePattern.push(randomChosenColor);
-    var buttonElem = $("div#" + randomChosenColor);
-    buttonElem.on("click", function(){
-        playSound(randomChosenColor);
-        buttonElem.fadeOut("fast", function(){
-            buttonElem.show();
-        });
-    });
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
 }
 
 function playSound(color)
 {
     var sound = new Audio("assets/sounds/" + color + ".mp3");
-        sound.play();
+    sound.play();
 }
